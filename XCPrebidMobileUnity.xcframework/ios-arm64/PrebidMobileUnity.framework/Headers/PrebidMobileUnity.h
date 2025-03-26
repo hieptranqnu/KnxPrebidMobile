883f10c0fc418b69745d30517908ae5736fdf21f
Hiep Tran <hieptranqnu@gmail.com>
2025-03-26 11:20:41 +0700

    Add files via upload

---
+++ XCPrebidMobileUnity.xcframework/ios-arm64/PrebidMobileUnity.framework/Headers/PrebidMobileUnity.h
@@ -0,0 +1,18 @@
+//
+//  PrebidMobileUnity.h
+//  PrebidMobileUnity
+//
+//  Created by Knorex on 2/20/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrebidMobileUnity.
+FOUNDATION_EXPORT double PrebidMobileUnityVersionNumber;
+
+//! Project version string for PrebidMobileUnity.
+FOUNDATION_EXPORT const unsigned char PrebidMobileUnityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrebidMobileUnity/PublicHeader.h>
+
+
